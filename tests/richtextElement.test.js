9d65fc8edad46f7fb1bea9daaf093be7c45d1219
Matt Nield <mattnield@gmail.com>
2025-05-08 15:51:32 +0100

    Remove console.log

--- tests/richtextElement.test.js
+++ tests/richtextElement.test.js
@@ -7,7 +7,6 @@ describe('richtextElement (data-driven)', () => {
   testCases.forEach(({ description, elementDef, elementVal, contentTypes, modularContent, validExpected }) => {
 
     it(description, () => {
-      console.log(description);
       const errors = validateRichTextElement(elementDef, elementVal, contentTypes, modularContent);
       if (errors.length > 0) console.error(errors);
       expect(errors.length === 0).toBe(validExpected);
